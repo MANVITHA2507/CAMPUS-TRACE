6a9451f696c6dc0444a42343a8b7866cae6968ae
NIMMAKAYALA MANVITHA <manvithamanvitha902@gmail.com>
2026-06-21 14:28:14 +0530

    Fix MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/campus_trace')
+mongoose.connect(process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/campus_trace')
   .then(() => console.log('MongoDB Connected!'))
   .catch(err => console.log(err));
 
